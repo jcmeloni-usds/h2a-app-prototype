d600edccba3d3d46536a1858fb2d3f5ee783a804
Julie Meloni <julie_c_meloni@omb.eop.gov>
2018-06-13 07:27:04 -0400

    placeholder text and ch1

--- js/components/Introduction.jsx
+++ js/components/Introduction.jsx
@@ -17,11 +17,46 @@ class Introduction extends React.Component {
   render() {
     return (
       <div className="schemaform-intro">
-        <FormTitle title="Create test form with U.S. Forms System"/>
-        <p>Scaffolding to get any form started!</p>
+        <FormTitle title="H-2A Visa Program"/>
+        
+        <p>[Some lovely intro text about what this is.]</p>
+        
+        <h4>Follow the steps below to [...].</h4>
+
+        <div className="process schemaform-process">
+          <ol>
+            <li className="process-step list-one">
+              <div><h5>Prepare</h5></div>
+              <div><h6>To fill out this application, you’ll need:</h6></div>
+              <ul>
+                <li>[....]</li>
+              </ul>
+              <p><strong>What if I need help filling out my application?</strong> [text here]</p>
+            </li>
+            <li className="process-step list-two">
+              <div><h5>Apply</h5></div>
+              <p>Complete this H-2A Application Form.</p>
+              <p>After submitting the form, [....]</p>
+            </li>
+            <li className="process-step list-three">
+              <div><h5>[Agency] Review</h5></div>
+              <p>[....]</p>
+            </li>
+            <li className="process-step list-four">
+              <div><h5>[Even More Agency] Reviews</h5></div>
+              <p>[....]</p>
+            </li>
+            <li className="process-step list-five">
+              <div><h5>Decision</h5></div>
+              <p>[....]</p>
+            </li>
+          </ol>
+
+        </div>
+
         <ProgressButton
           onButtonClick={this.startForm}
-          buttonText="Start Form"
+          buttonText="Start H-2A Visa Program Application"
           buttonClass="usa-button-primary schemaform-start-button"
           afterText="»"/>
       </div>
--- js/config/form.js
+++ js/config/form.js
@@ -1,12 +1,13 @@
 import Introduction from '../components/Introduction.jsx';
 import fullName from '../definitions/fullName';
-
 import fullNameUI from 'us-forms-system/lib/js/definitions/fullName';
 
+
+
 const formConfig = {
-  title: 'Form',
-  subTitle: 'Test',
-  formId: '',
+  title: 'H-2A Visa Program',
+  subTitle: 'Consolidated Application Prototype',
+  formId: 'OMBxxxx',
   urlPrefix: '/',
   trackingPrefix: 'form-',
   transformForSubmit: '',
@@ -14,27 +15,133 @@ const formConfig = {
   introduction: Introduction,
   confirmation: '',
   defaultDefinitions: {
-    fullName
+    // stuff from lib
   },
+
+
+//TODO - do validations that kick error messages
+//TODO - align types with built-in widgets
+//TODO - use lib where appropriate, e.g. fullname, address
+//TODO - dedupe into definition files for easier use in the conditional paths
+
+
   chapters: {
-    firstSection: {
-      title: 'First Section',
+    aboutPetition: {
+      title: 'About this Petition',
       pages: {
-        firstPage: {
-          path: 'first-section/first-page',
-          title: 'First Page',
-          uiSchema: {
-            fullName: fullNameUI
-          },
+        petitionInformation: {
+          path: 'about-petition/petition-information',
+          title: 'Petition Information',
+          required: [
+            'employerType',
+            'employerNumber',
+            'numberWorkersRequested',
+            'workersNamed',
+            'usingFLCorOther'
+          ],
+
           schema: {
             type: 'object',
+            required: [
+              'employerType',
+              'employerNumber',
+              'numberWorkersRequested',
+              'workersNamed',
+              'usingFLCorOther'
+            ],
             properties: {
-              fullName
+              employerType: {
+                title: 'What type of employer is seeking workers?',
+                type: 'string',
+                default: 'Individual Employer',
+                //TODO: Add the remaining options (they kick conditionals)
+                enum: ['Individual Employer'],
+                enumNames: ['Individual Employer']
+              }, 
+              employerNumber: {
+                title: 'How Many Employers are There?',
+                type: 'integer',
+                default: 1
+                //TODO: Check if limit; use range if necessary
+              },
+              numberWorkersRequested: {
+                title: 'Number of Workers Requested',
+                type: 'integer',
+                default: 1
+                //TODO: Check if limit; use range if necessary
+              }, 
+              workersNamed: {
+                title: 'Are the Workers Named?',
+                type: 'string',
+                default: 'No',
+                enum: ['Yes','No'],
+                enumNames: ['Yes','No']
+                //TODO: Implement non-happy-path conditional
+              },     
+              usingFLCorOther: {
+                title: 'Is the employer using a Farm Labor Contractor (FLC), placement service, or other agent to recruit, supervise, transport, house, and/or pay for these workers?',
+                type: 'string',
+                default: 'No',
+                enum: ['Yes','No'],
+                enumNames: ['Yes','No']
+                //TODO: Implement non-happy-path conditional
+              },
             }
+          },
+
+          uiSchema: {
+            employerType: {
+              'ui:title': 'What type of employer is seeking workers?',
+              'ui:description': '[Helper text here]',
+              'ui:widget': 'select',
+              'ui:validations': '',
+              'ui:errorMessages': {
+                 pattern: 'Please select the type of employer.'
+              },
+            }, 
+            employerNumber: {
+              'ui:title': 'How Many Employers are There?',
+              'ui:description': '[Helper text here]',
+              'ui:widget': 'updown',
+              'ui:validations': '',
+              'ui:errorMessages': {
+                 pattern: 'Please select the number of employers on this petition.'
+              },
+            }, 
+            numberWorkersRequested: {
+              'ui:title': 'Number of Workers Requested',
+              'ui:description': '[Helper text here]',
+              'ui:widget': 'updown',
+              'ui:validations': '',
+              'ui:errorMessages': {
+                 pattern: 'Please enter the number of workers you are requesting.'
+              },
+            }, 
+            workersNamed: {
+              'ui:title': 'Are the Workers Named?',
+              'ui:description': '[Helper text here]',
+              'ui:widget': 'radio',
+              'ui:validations': '',
+              'ui:errorMessages': {
+                 pattern: 'Please indicate if your petition includes named workers.'
+              },
+             },     
+            usingFLCorOther: {
+              'ui:title': 'Is the employer using a Farm Labor Contractor (FLC), placement service, or other agent to recruit, supervise, transport, house, and/or pay for these workers?',
+              'ui:description': '[Helper text here]',
+              'ui:widget': 'radio',
+              'ui:validations': '',
+              'ui:errorMessages': {
+                 pattern: 'Please indicate if you are using a Farm Labor Contractor (FLC), placement service, or other agent to recruit, supervise, transport, house, and/or pay for these workers.'
+              }     
+            }                 
           }
         },
+
+
+
         secondPage: {
-          path: 'first-section/second-page',
+          path: 'about-petition/second-page',
           title: 'Second Page',
           uiSchema: {},
           schema: {
@@ -44,10 +151,153 @@ const formConfig = {
         }
       }
     },
-    secondSection: {
-      title: 'Second Section',
-      pages: {
 
+
+/*
+Confirm Visa Type   
+
+RADIO   Are you seeking Temporary Agricultural Worker Visas (H-2A Visas) for these workers?
+SELECT  Basis for the visa classification supported by this application
+SELECT  Requested Action
+
+*/
+
+
+
+
+    aboutPetitioner: {
+      title: 'About Yourself',
+      pages: {
+        firstPage: {
+          path: 'about-petitioner/first-page',
+          title: 'First Page',
+          uiSchema: {
+            //fullName: fullNameUI
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              //fullName
+            }
+          }
+        },
+        secondPage: {
+          path: 'about-petitioner/second-page',
+          title: 'Second Page',
+          uiSchema: {},
+          schema: {
+            type: 'object',
+            properties: {}
+          }
+        }
+      }
+    },
+    aboutBusiness: {
+      title: 'About the Business',
+      pages: {
+        firstPage: {
+          path: 'about-business/first-page',
+          title: 'First Page',
+          uiSchema: {
+            //fullName: fullNameUI
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              //fullName
+            }
+          }
+        },
+        secondPage: {
+          path: 'about-business-page',
+          title: 'Second Page',
+          uiSchema: {},
+          schema: {
+            type: 'object',
+            properties: {}
+          }
+        }
+      }
+    },
+    aboutJob: {
+      title: 'About the Job',
+      pages: {
+        firstPage: {
+          path: 'about-job/first-page',
+          title: 'First Page',
+          uiSchema: {
+            //fullName: fullNameUI
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              //fullName
+            }
+          }
+        },
+        secondPage: {
+          path: 'about-job/second-page',
+          title: 'Second Page',
+          uiSchema: {},
+          schema: {
+            type: 'object',
+            properties: {}
+          }
+        }
+      }
+    },
+    aboutForeignWorkers: {
+      title: 'About the Foreign Workers',
+      pages: {
+        firstPage: {
+          path: 'about-foreign-workers/first-page',
+          title: 'First Page',
+          uiSchema: {
+            //fullName: fullNameUI
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              //fullName
+            }
+          }
+        },
+        secondPage: {
+          path: 'about-foreign-workers/second-page',
+          title: 'Second Page',
+          uiSchema: {},
+          schema: {
+            type: 'object',
+            properties: {}
+          }
+        }
+      }
+    },
+    aboutUSWorkers: {
+      title: 'About the American Workers',
+      pages: {
+        firstPage: {
+          path: 'about-us-workers/first-page',
+          title: 'First Page',
+          uiSchema: {
+            //fullName: fullNameUI
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              //fullName
+            }
+          }
+        },
+        secondPage: {
+          path: 'about-us-workers/second-page',
+          title: 'Second Page',
+          uiSchema: {},
+          schema: {
+            type: 'object',
+            properties: {}
+          }
+        }
       }
     }
   }
